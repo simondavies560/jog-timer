0cf8d1d9bd8e32fbe08bc762f8e5e196cc2cf420
simondavies560 <36174660+simondavies560@users.noreply.github.com>
2026-07-14 16:20:29 +1000

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,26 @@
+const CACHE = 'jogwalk-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', (e) => {
+  self.skipWaiting();
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.filter(k => k !== CACHE).map(k => caches.delete(k))
+    ))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request))
+  );
+});
